78d920f09c46ff44398cacc9f4bfff30e1a4f8b3
Vipul Goyal <vipulkg30@gmail.com>
2022-06-09 18:43:39 +0530

    Initial Commit

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,40 @@
+import { useContext, useEffect, useState } from "react";
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import axios from "axios";
+// import { Posts } from "../../dummyData";
+import { AuthContext } from "../../context/AuthContext";
+
+export default function Feed({username}) {
+  const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = username
+        ? await axios.get("/posts/profile/" + username)
+        : await axios.get("posts/timeline/" + user._id);
+        // : await axios.get("posts/timeline/622d9409dcd9229d273e69e3");
+        setPosts(
+          res.data.sort((p1, p2) => {
+            return new Date(p2.createdAt) - new Date(p1.createdAt);
+          })
+        );
+        // .then(res => console.log(res))
+      // .catch(err => console.log(err));
+    };
+    fetchPosts();
+  }, [username, user._id]);
+
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        {(!username || username === user.username) && <Share />}
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
+        ))}
+      </div>
+    </div>
+  );
+}
